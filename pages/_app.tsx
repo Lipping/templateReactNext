548c3a7fee733dc7f2a6193d981afd7df6dafa4d
Lipping <felipelagesstefenoni@gmail.com>
2022-08-03 18:34:57 -0300

    initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import createCache from '@emotion/cache'
+import { CacheProvider } from '@emotion/react'
+import { AppProps } from 'next/app'
+import React from 'react'
+
+import { globalStyles } from '../shared/styles'
+
+const cache = createCache({ key: 'next' })
+
+export default function App({ Component, pageProps }:AppProps) {
+  return (
+    <CacheProvider value={cache}>
+      {globalStyles}
+      <Component {...pageProps} />
+    </CacheProvider>
+  )
+}
